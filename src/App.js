7b1005977f09bf64e26dff82c369758011d2246c
Daniil <daniilfgs@gmail.com>
2021-02-14 20:46:15 +0400

    План действий с роутом в React 1)npm install react-router-dom 2)Обернуть в index js App тегом BrowserRouter 3)В App начать с тега Swicth  в нем теги Route с прописанными адресами 4) По адресу / добавить параметр exact

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route,Switch} from "react-router-dom";
+import General from "./routes/General/General";
+import Courses from "./routes/Courser/Courses";
+import Shop from "./routes/Shop/Shop";
+import News from "./routes/News/News";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Switch>
+            <Route path="/" exact component={General}/> //добавить параметр exact чтобы можно было делать роут по другим адресам
+            <Route path="/shop" component={Shop}/>
+            <Route path="/courses" component={Courses}/>
+            <Route path="/news" component={News}/>
+        </Switch>
+    );
 }
 
 export default App;
