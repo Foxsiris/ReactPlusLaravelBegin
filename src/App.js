9d8e4530b629afd32aa5026995144c24a582c09f
Daniil <daniilfgs@gmail.com>
2021-02-14 22:03:44 +0400

    Добавил стейт , редусер , контекст , пока не понятно как это работает , потомо когда нибудь буду смотреть этот код может разбирусь , но лучше выучить Redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 import {Route,Switch} from "react-router-dom";
 import General from "./routes/General/General";
 import Courses from "./routes/Courser/Courses";
 import Shop from "./routes/Shop/Shop";
 import News from "./routes/News/News";
+import ContexData from "./context/Data/ContextData";
+import ReducerData from "./context/Data/ReducerData";
+import StateData from "./context/Data/StateData";
 
 function App() {
+    const [stateData,dispatchData] = React.useReducer(ReducerData,StateData)
     return (
         <Switch>
-            <Route path="/" exact component={General}/> //добавить параметр exact чтобы можно было делать роут по другим адресам
-            <Route path="/shop" component={Shop}/>
-            <Route path="/courses" component={Courses}/>
-            <Route path="/news" component={News}/>
+            <ContexData.Provider value={{stateData,dispatchData}}>
+                <Route path="/" exact component={General}/>
+                <Route path="/shop" component={Shop}/>
+                <Route path="/courses" component={Courses}/>
+                <Route path="/news" component={News}/>
+            </ContexData.Provider>
         </Switch>
     );
 }
--- src/routes/General/General.jsx
+++ src/routes/General/General.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import ContexData from "../../context/Data/ContextData";
+
 
 function General(){
+    const {stateData} = React.useContext(ContexData)
+    console.log(stateData)
     return(
         <div>
             General
