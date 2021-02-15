a537cf852f63064d981504610e850c704de84c63
Daniil <daniilfgs@gmail.com>
2021-02-15 18:46:21 +0400

    Сделал отображение новстей с помощью fetch который обращаеться к бэку на laravel .

--- src/routes/News/News.js
+++ src/routes/News/News.js
@@ -2,9 +2,30 @@ import React from "react";
 import classes from "../../style.module.scss";
 import Navbar from "../../modules/Navigation/Navbar";
 import Header from "../../modules/Navigation/Header";
+import ContexData from "../../context/Data/ContextData";
 
-function  News(){
-    return(
+function News() {
+    const {stateData,dispatchData} = React.useContext(ContexData)
+    React.useEffect(() => {
+        const fecthNews = async () => {
+            try {
+                const response = await fetch('http://127.0.0.1:8000/news')
+                if (response.status === 200) {
+                    const result = response.json().then(data=>{
+                        dispatchData({
+                            type:"FETCH_NEWS",
+                            payload:data
+                        })
+                    })
+
+                }
+            } catch (e) {
+                console.log(e)
+            }
+        }
+        fecthNews()
+    }, [])
+    return (
         <div>
             <div className={classes.main}>
                 <div className={classes.main_navigation}>
@@ -12,6 +33,19 @@ function  News(){
                 </div>
                 <div className={classes.main_content}>
                     <Header/>
+                    {
+                        stateData.news.map((el,index)=>{
+                            return(
+                                <div key={index}>
+                                    <div>{el.title}</div>
+                                    <div>{el.description}</div>
+                                    <div>
+                                        <img src={el.img} alt=""/>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
             </div>
         </div>
